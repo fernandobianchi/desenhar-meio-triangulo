c0ee6e27533d1b595276571e67cd08e297d6efb4
Fernando Bianchi <fernandobianchi@hotmail.it>
2020-09-02 22:28:50 -0300

    Create desenharMeioTriangulo.cpp

---
+++ desenharMeioTriangulo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<locale>
+using namespace std;
+int main ()
+{
+	setlocale(LC_ALL, "ptb");
+	int desenha, totLinhas, linAtual;
+	cout<<"Quantas linhas deve ter o triângulo? ";
+	cin>>totLinhas;
+	for (linAtual=1;linAtual<=totLinhas;linAtual++)
+		{
+			cout<<"\n";
+			for (desenha=1;desenha<=linAtual;desenha++)
+				{
+					cout<<"*";
+				}
+		}
+}
